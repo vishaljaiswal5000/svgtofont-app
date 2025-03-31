928867f8bb24c67c8b6a32d0dd0e8973ad4d6479
vishaljaiswal5000 <vishaljaiswal5000@gmail.com>
2025-03-31 14:28:57 +1100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <h1>SVG to font app</h1>
+      <div className="grid-box">
+      <div className="grid-box__item">  <i className="icon-aperture" /> <span>icon-aperture</span></div>
+      <div className="grid-box__item">  <i className="icon-facebook" /> <span>icon-facebook</span></div>
+      <div className="grid-box__item">  <i className="icon-fast-forward" /> <span>icon-fast-forward</span></div>
+      <div className="grid-box__item">  <i className="icon-instagram" /> <span>icon-instagram</span></div>
+      <div className="grid-box__item">  <i className="icon-linkedin" /> <span>icon-linkedin</span></div>
+      <div className="grid-box__item">  <i className="icon-mail" /> <span>icon-mail</span></div>
+      <div className="grid-box__item">  <i className="icon-youtube" /> <span>icon-youtube</span></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
